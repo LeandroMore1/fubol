57b1437950625845b2df89afa28a49765f10c369
Leandro More <manumore42@gmail.com>
2023-03-31 23:34:05 -0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,112 @@
+import { useState,useEffect } from "react"
+import { pibes } from "../dataBase.js"
+import chapo from "./chapo.png"
+import lore from "./lore.png"
+import fran from"./fran.png"
+import pes from "./pes.png"
+import joaco from "./joaco.png"
+import drupi from "./drupi.gif"
+import manolo from "./manu.png"
+import pulpo from "./pulpo.png"
+import monke from "./monke.gif"
+import "../components/Header.css"
+
+export const Header = () => {
+
+    const [muchachos, setMuchachos] = useState([])
+    
+    let nomb = muchachos.map(el=> el.nombre)
+    let bestPlayer = muchachos.find(el=> Math.max(el.mvp))
+    let i = 1 
+    
+    
+    const compare = (a, b) => {
+        if (a.goles < b.goles) {
+            return 1;
+        }
+        if (a.goles > b.goles) {
+            return -1;
+        }
+        return 0;
+    }
+        
+    const obtenerLista= () =>{
+        const data = Promise.resolve(pibes)
+
+        data.then((el) => setMuchachos(el.sort(compare)))
+    }
+    useEffect(()=>{
+            obtenerLista();
+    },[muchachos])
+
+    
+
+
+    return (
+
+        <>
+        <div className="contTitle">
+            <h1>Defensores del kush</h1>
+            <img className="monke" src={monke} alt="" />
+            </div>
+            <div className="containerFotos container">
+            <img src={drupi} alt="" />
+            <img src={chapo} alt="" />
+            <img src={joaco}alt="" />
+            <img src={lore} alt="" />
+            <img src={pes} alt="" />
+            <img src={manolo} alt="" />
+            <img src={fran} alt="" />
+            <img src={pulpo} alt="" />
+            </div>
+           <h1>Tabla de posiciones:</h1>
+            <div className="containersub">
+                <h2>Goleador 🦍: </h2>
+                <img className="imgSub" src={chapo} alt="" />
+                <h2>El muerto 🐕: </h2>
+                <img className="imgSub" src={lore} alt="" />
+                <h2>Mejor jugador 🏆: </h2>
+                <img className="imgSub" src={chapo} alt="" />
+            </div>
+          
+            <div className="container tabla">
+
+
+
+                <table className="table" >
+                    <thead>
+                        <tr>
+                            <th>Pos.</th>
+                            <th>Nombre</th>
+                            <th>Goles ⚽️</th>
+                            <th>Ganó 🦍</th>
+                            <th>Perdió 🥶</th>
+                            <th>MVP 🏅</th>
+                        </tr>
+                    </thead>
+                    {
+                        muchachos.map(jugador => (
+                            <tbody key={jugador.id}>
+                                <tr>
+
+                                    <td className="d-flex">{i++}{i - 1 === 1 ? " 🥇" : i - 1 === 2 ? " 🥈" : i - 1 === 3 ? " 🥉" : i - 1 === 7 ? " 👻" : <p></p>}</td>
+                                    <td>{jugador.nombre}</td>
+                                    <td>{jugador.goles}</td>
+                                    <td>{jugador.ganados}</td>
+                                    <td>{jugador.perdidos}</td>
+                                    {jugador.mvp > 0 ?
+                                        <td>{jugador.mvp} ⭐️</td> : <td>{jugador.mvp}</td>
+                                    }
+                                </tr>
+
+                            </tbody>
+                        ))
+                    }
+
+                </table>
+            </div>
+
+        </>
+
+    )
+}                   
---
+++ src/dataBase.js
@@ -0,0 +1,11 @@
+
+export const pibes = [ 
+{id: 1, nombre: "Manolo", goles: 4,  ganados: 1,  perdidos: 1, mvp: 0 },
+{id: 2,nombre: "Fran", goles: 11,  ganados: 2,  perdidos: 1, mvp: 0},
+{id: 3,nombre: "Pulpo", goles: 10,  ganados: 2,  perdidos: 2, mvp: 1},
+{id: 4,nombre: "Joaco", goles: 4,  ganados: 2,  perdidos: 1, mvp: 0},
+{id: 5,nombre: "Lore", goles: 2,  ganados: 0,  perdidos: 1, mvp: 0},
+{id: 6,nombre: "Pes", goles: 14,  ganados: 2,  perdidos: 2, mvp: 0},
+{id: 7,nombre: "Chapo", goles: 15,  ganados: 3,  perdidos: 1, mvp: 1}
+]
+
