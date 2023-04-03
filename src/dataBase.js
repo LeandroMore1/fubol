3773f20a185d27b7070faeac03df01c967a1c2a6
Leandro More <manumore42@gmail.com>
2023-04-03 13:23:22 -0300

    agregacion drupi

--- src/dataBase.js
+++ src/dataBase.js
@@ -6,6 +6,7 @@ export const pibes = [
 {id: 4,nombre: "Joaco", goles: 4,  ganados: 2,  perdidos: 1, mvp: 0},
 {id: 5,nombre: "Lore", goles: 2,  ganados: 0,  perdidos: 1, mvp: 0},
 {id: 6,nombre: "Pes", goles: 14,  ganados: 2,  perdidos: 2, mvp: 0},
-{id: 7,nombre: "Chapo", goles: 15,  ganados: 3,  perdidos: 1, mvp: 1}
+{id: 7,nombre: "Chapo", goles: 15,  ganados: 3,  perdidos: 1, mvp: 1},
+{id: 8,nombre: "Drupi", goles: 5,  ganados: 1,  perdidos: 0, mvp: 0}
 ]
 
