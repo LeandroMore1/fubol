55481c2fc4c00b20243f87bf3051224ebc7dd1d9
Leandro More <manumore42@gmail.com>
2023-04-11 23:46:40 -0300

    actualizacion de tabla

--- src/dataBase.js
+++ src/dataBase.js
@@ -1,14 +1,14 @@
 
 export const pibes = [ 
-{id: 1, nombre: "Manolo", goles: 6,  ganados: 2,  perdidos: 1, mvp: 0 },
+{id: 1, nombre: "Manolo", goles: 7,  ganados: 2,  perdidos:2, mvp: 0 },
 {id: 2,nombre: "Fran", goles: 11,  ganados: 2,  perdidos: 1, mvp: 0},
 {id: 3,nombre: "Pulpo", goles: 10,  ganados: 2,  perdidos: 2, mvp: 1},
 {id: 4,nombre: "Joaco", goles: 4,  ganados: 2,  perdidos: 1, mvp: 0},
-{id: 5,nombre: "Lore", goles: 2,  ganados: 0,  perdidos: 1, mvp: 0},
-{id: 6,nombre: "Pes", goles: 24,  ganados: 3,  perdidos: 2, mvp: 1},
-{id: 7,nombre: "Chapo", goles: 20,  ganados: 3,  perdidos:2, mvp: 1},
+{id: 5,nombre: "Lore", goles: 9,  ganados: 1,  perdidos: 1, mvp: 1},
+{id: 6,nombre: "Pes", goles: 29,  ganados: 3,  perdidos:3, mvp: 1},
+{id: 7,nombre: "Chapo", goles: 28,  ganados: 3,  perdidos:3, mvp: 1},
 {id: 8,nombre: "Drupi", goles: 8,  ganados: 2,  perdidos: 0, mvp: 0},
-{id: 9,nombre: "Juli", goles: 0,  ganados: 0,  perdidos: 0, mvp: 0}
+{id: 9,nombre: "Juli", goles: 3,  ganados: 1,  perdidos: 0, mvp: 0}
 ]
 
                     
