f8eea33232884c064395806982ab67db83478c21
Leandro More <manumore42@gmail.com>
2023-05-22 20:10:24 -0300

    actualizacion

--- src/dataBase.js
+++ src/dataBase.js
@@ -5,7 +5,7 @@ export const pibes = [
 {id: 3,nombre: "Pulpo", goles: 12,  ganados: 2,  perdidos: 3, mvp: 1},
 {id: 4,nombre: "Joaco", goles: 8,  ganados: 2,  perdidos: 2, mvp: 0},
 {id: 5,nombre: "Lore", goles: 9,  ganados: 1,  perdidos: 1, mvp: 1},
-{id: 6,nombre: "Pes", goles: 41,  ganados: 5,  perdidos:4, mvp: 2},
+{id: 6,nombre: "Pes", goles: 44,  ganados: 5,  perdidos:4, mvp: 2},
 {id: 7,nombre: "Chapo", goles:53,  ganados: 4,  perdidos:6, mvp: 1},
 {id: 8,nombre: "Drupi", goles: 34,  ganados: 6,  perdidos: 0, mvp: 1},
 {id: 9,nombre: "Juli", goles: 12,  ganados: 2,  perdidos:1, mvp: 0}
