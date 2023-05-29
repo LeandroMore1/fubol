6fa819a7ef8065f4a1908d2e0e4d4345ba87e5aa
Leandro More <manumore42@gmail.com>
2023-05-29 14:45:26 -0300

    actualizacion

--- src/dataBase.js
+++ src/dataBase.js
@@ -1,14 +1,14 @@
 
 export const pibes = [ 
-{id: 1, nombre: "Manolo", goles: 14,  ganados: 4,  perdidos:4, mvp: 0 },
-{id: 2,nombre: "Fran", goles: 14,  ganados: 2,  perdidos: 2, mvp: 0},
-{id: 3,nombre: "Pulpo", goles: 12,  ganados: 2,  perdidos: 3, mvp: 1},
-{id: 4,nombre: "Joaco", goles: 8,  ganados: 2,  perdidos: 2, mvp: 0},
-{id: 5,nombre: "Lore", goles: 14,  ganados: 2,  perdidos: 1, mvp: 1},
-{id: 6,nombre: "Pes", goles: 47,  ganados: 5,  perdidos:5, mvp: 2},
-{id: 7,nombre: "Chapo", goles:57,  ganados: 4,  perdidos:7, mvp: 1},
-{id: 8,nombre: "Drupi", goles: 34,  ganados: 6,  perdidos: 0, mvp: 1},
-{id: 9,nombre: "Juli", goles: 12,  ganados: 2,  perdidos:1, mvp: 0}
+{id: 1, nombre: "Manolo", goles: 17,  ganados: 4,  perdidos:5, mvp: 0,img:"./components/manu.jpeg" },
+{id: 2,nombre: "Fran", goles: 14,  ganados: 2,  perdidos: 2, mvp: 0,img:"./components/fran.png"},
+{id: 3,nombre: "Pulpo", goles: 12,  ganados: 2,  perdidos: 3, mvp: 1,img:"./components/pulpo.png"},
+{id: 4,nombre: "Joaco", goles: 8,  ganados: 2,  perdidos: 2, mvp: 0,img:"./components/joaco.png"},
+{id: 5,nombre: "Lore", goles: 14,  ganados: 2,  perdidos: 1, mvp: 1,img:"./components/lore.png"},
+{id: 6,nombre: "Pes", goles: 51,  ganados: 5,  perdidos:6, mvp: 2,img:"./components/pes.png"},
+{id: 7,nombre: "Chapo", goles:59,  ganados:5,  perdidos:7, mvp: 1,img:"./components/chapo.png"},
+{id: 8,nombre: "Drupi", goles: 38,  ganados: 7,  perdidos: 0, mvp: 1,img:"./components/drupi.gif"},
+{id: 9,nombre: "Juli", goles: 16,  ganados: 3,  perdidos:1, mvp: 0,img:"./components/juli.png"}
 ]
 
                     
